d38b59693bbb1dcb43fdab3a586361007b408c78
xiaoxiongwn <81366890+xiaoxiongwn@users.noreply.github.com>
2026-05-03 19:25:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=f1c796c3-db21-4248-8530-7af2169a22b6",
     # 添加更多链接
 ]
 
